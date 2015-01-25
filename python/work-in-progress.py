ad2acb488c482a6189eece53c7bcfcabef5c58d0
Marcus Rugger <marcusrugger@yahoo.com>
2015-01-25 15:12:38 -0500

    Reorganized a bit Removed idle_loop print statement

--- python/work-in-progress.py
+++ python/work-in-progress.py
@@ -4,15 +4,6 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
-LED_STATE=True
-SWITCH_STATE=GPIO.HIGH
-
-CHANNEL_SWITCH=2
-CHANNEL_LED=4
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(CHANNEL_SWITCH, GPIO.IN)
-GPIO.setup(CHANNEL_LED, GPIO.OUT)
 
 def set_led(state):
     global LED_STATE
@@ -20,24 +11,30 @@ def set_led(state):
     print 'New LED state: {0}'.format(LED_STATE)
     GPIO.output(CHANNEL_LED, LED_STATE)
 
+
 def toggle_led():
     set_led(not(LED_STATE))
+
 
 def button_changed_state(state):
     if state == GPIO.LOW:
         toggle_led()
 
+
 def idle_loop():
     global SWITCH_STATE
 
+    while True:
+        time.sleep(0.2)
+        input_state = GPIO.input(CHANNEL_SWITCH)
+        if input_state != SWITCH_STATE:
+            SWITCH_STATE=input_state
+            button_changed_state(input_state)
+
+
+def run_application():
     try:
-        while True:
-            print("Sleeping...")
-            time.sleep(0.2)
-            input_state = GPIO.input(CHANNEL_SWITCH)
-            if input_state != SWITCH_STATE:
-                SWITCH_STATE=input_state
-                button_changed_state(input_state)
+        idle_loop()
 
     except:
         e = sys.exc_info()[0]
@@ -45,9 +42,21 @@ def idle_loop():
         pass
 
 
-set_led(True)
+def initialize_application():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(CHANNEL_SWITCH, GPIO.IN)
+    GPIO.setup(CHANNEL_LED, GPIO.OUT)
+    set_led(True)
 
-idle_loop()
+
+LED_STATE=True
+SWITCH_STATE=GPIO.HIGH
+
+CHANNEL_SWITCH=2
+CHANNEL_LED=4
+
+initialize_application()
+run_application()
 
 GPIO.cleanup()
 print("All done.  Goodbye.");
