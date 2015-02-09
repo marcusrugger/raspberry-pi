3a64277dc7f66b0fad8d87c87c9364c6e0311bea
Marcus Rugger <marcusrugger@yahoo.com>
2015-02-09 18:38:42 -0500

    Refactor i2c

--- i2c/sample.py
+++ i2c/sample.py
@@ -1,60 +1,65 @@
 #!/usr/bin/env python
 
-import io
+import os
 import fcntl
 
 I2C_SLAVE=0x0703
 
 class i2c:
 
-   def __init__(self, device, bus):
+   def __init__(self, bus, address):
+      self.bus = bus
+      self.address = address
 
-      self.fr = io.open("/dev/i2c-"+str(bus), "rb", buffering=0)
-      self.fw = io.open("/dev/i2c-"+str(bus), "wb", buffering=0)
+   def __enter__(self):
+      self.fd = os.open("/dev/i2c-"+str(self.bus), O_RDWR | O_SYNC)
+      fcntl.ioctl(self.fd, I2C_SLAVE, self.address)
+      return self
 
-      # set device address
+   def __exit__(self, type, value, traceback):
+      os.close(self.fd)
 
-      fcntl.ioctl(self.fr, I2C_SLAVE, device)
-      fcntl.ioctl(self.fw, I2C_SLAVE, device)
+   def readByte():
+      return os.read(self.fd, 1)
 
-   def write(self, bytes):
-      self.fw.write(bytes)
+   def readWord():
+      return os.read(self.fd, 2)
 
-   def read(self, bytes):
-      return self.fr.read(bytes)
+   def readBytes(count):
+      return os.read(self.fd. count)
 
-   def close(self):
-      self.fw.close()
-      self.fr.close()
+   def writeBytes(bytes):
+      os.write(self.fd, bytes)
 
-if __name__ == "__main__":
 
-   import time
-   import struct
+# if __name__ == "__main__":
 
-   import i2c
+#    import time
+#    import struct
 
-   dev = i2c.i2c(0x53, 1) # device 0x53, bus 1
+#    import i2c
 
-   dev.write("\x2D\x00") # POWER_CTL reset
-   dev.write("\x2D\x08") # POWER_CTL measure
-   dev.write("\x31\x00") # DATA_FORMAT reset
-   dev.write("\x31\x0B") # DATA_FORMAT full res +/- 16g
+#    dev = i2c.i2c(0x53, 1) # device 0x53, bus 1
 
-   num_samples=5000
+#    dev.write("\x2D\x00") # POWER_CTL reset
+#    dev.write("\x2D\x08") # POWER_CTL measure
+#    dev.write("\x31\x00") # DATA_FORMAT reset
+#    dev.write("\x31\x0B") # DATA_FORMAT full res +/- 16g
 
-   sample=[(0,0,0)]*num_samples
+#    num_samples=5000
 
-   start1 = time.time()
+#    sample=[(0,0,0)]*num_samples
 
-   for s in xrange(num_samples):
+#    start1 = time.time()
 
-      dev.write("\x32")
-      sample[s] = struct.unpack('3h', dev.read(6))
-      print("x={} y={} z={}".format(sample[s][0], sample[s][1], sample[s][2]))
+#    for s in xrange(num_samples):
 
-   end1 = time.time()
+#       dev.write("\x32")
+#       sample[s] = struct.unpack('3h', dev.read(6))
+#       print("x={} y={} z={}".format(sample[s][0], sample[s][1], sample[s][2]))
 
-   print(end1-start1)
+#    end1 = time.time()
 
-   dev.close()
+#    print(end1-start1)
+
+#    dev.close()
